c0a5b7bab6a6a2a2d9d70c49c8d8cde980a2320d
mitul.sonagara <mitul.sonagara@drcsystems.com>
2025-10-14 17:04:18 +0530

    initial commit

---
+++ backend/src/config/config.ts
@@ -0,0 +1,29 @@
+// src/config/config.ts
+import dotenv from "dotenv";
+import path from "path";
+
+// Load environment variables from .env file
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+export const config = {
+  port: process.env.PORT || 3000,
+  mongodb: {
+    uri: process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase",
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || "default-secret-key",
+    expiresIn: process.env.JWT_EXPIRES_IN || "1h",
+  },
+  bcrypt: {
+    saltRounds: parseInt(process.env.SALT_ROUNDS || "10", 10),
+  },
+  email: {
+    host: process.env.EMAIL_HOST,
+    port: parseInt(process.env.EMAIL_PORT || "587", 10),
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  frontend: {
+    url: process.env.FRONTEND_URL,
+  },
+} as const;
